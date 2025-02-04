a117c823aa137aa9f92dd935796899a48e3ccbf3
Karthik Gopalan <gkarth@codeaurora.org>
2025-02-04 20:01:38 +0800

    miuicamera-sweet: Enable SVELTE memory configuration
    Enable SVELTE memory configuration.
    
    Change-Id: I338e41f97a1a416cbff36cd4728679ef
    Signed-off-by: Karthik Gopalan <gkarth@codeaurora.org>
    Signed-off-by: eidawon <projecteidawon@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,10 @@ include vendor/xiaomi/miuicamera-sweet/BoardConfigVendor.mk
 
 MIUICAMERA_PATH := device/xiaomi/miuicamera-sweet
 
+# Malloc
+MALLOC_SVELTE := true
+MALLOC_SVELTE_FOR_LIBC32 := true
+
 # Properties
 TARGET_SYSTEM_PROP += $(MIUICAMERA_PATH)/system.prop
 
